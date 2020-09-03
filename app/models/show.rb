6f9ebbda7da19fa807bd40acc6c310f7f2e75003
Christie Yoon <cleex1999@gmail.com>
2020-09-03 17:18:54 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,26 +5,25 @@ class Show < ActiveRecord::Base
     Show.all.maximum("rating")
   end
 
+  def most_popular_show
+  end
+
+  def lowest_rating
+  end
+
+  def least_popular_show
+  end
+
+  def ratings_sum
+    self.sum("ratings")
+  end
+
+  def popular_shows
+  end
+
+  def shows_by_alphabetical_order
+  end
 end
-  #
-  # def most_popular_show
-  # end
-  #
-  # def lowest_rating
-  # end
-  #
-  # def least_popular_show
-  # end
-  #
-  # def ratings_sum
-  #   self.sum("ratings")
-  # end
-  #
-  # def popular_shows
-  # end
-  #
-  # def shows_by_alphabetical_order
-  # end
 
 
 # create_table "shows", force: :cascade do |t|
